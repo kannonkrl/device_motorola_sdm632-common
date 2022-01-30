5cd83f3e8d5e08b5b0fab9476e641741001f26f4
MadhavSaladi <baaswanthmadhav@gmail.com>
2022-01-30 14:14:03 -0300

    sdm632-common: Add GoogleCameraGoPrebuilt
    - By Greatness
    - Changelog:
    
    - This is a special build with abnormal mechanisms in modification end. It's based on Camera Go 2.5.376972576 from Jiophone.
    - HDR, Nightsight and toggles like seen in google's demo
    - Nightsight Resolution increased to be like normal mode
    - Grid Lines and Selfie Mirroring Option Unhidden
    - 32 Bits Supported
    - Minimum Android Version 8
    - If you can, compare the hash of the downloaded file with the one below if you wanna make sure the apk is authentic by me. I don't bear responsibilities if you face issues from an apk with different hash.
    
    SHA256:
    b1935ca0cc6335df947041467ad07d7150d7cbd08f1eae628f12b0818a0f02ed
    
    Support Group: https://t.me/GcamGo
    
    Don't contact me in PM for support or bugs regarding this cam.
    
    Change-Id: I53f68cf8829ce76acee28cf7af9915bab97c16bc

--- common.mk
+++ common.mk
@@ -110,7 +110,12 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl:32 \
     libbson.vendor \
     libxml2 \
-    vendor.qti.hardware.camera.device@1.0:64
+    vendor.qti.hardware.camera.device@1.0:64 \
+    GoogleCameraGoPrebuilt
+
+# QTI Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
